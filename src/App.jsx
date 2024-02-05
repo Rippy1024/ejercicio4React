5bba0b59068d4b5baaff23ee1162d8a9f6e91831
Nano <luxorius07@gmail.com>
2024-02-04 22:43:30 -0300

    agregamos el maquetado del componente Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+  <>
+  <Container className="my-4">
+  <h1 className="text-center">Lista de Tareas</h1>
+  </Container>
+  <Footer></Footer>
+  </>
+  );
+  
 }
 
 export default App;
