f38a92f8aa250457f69f4808b35040434c0a0379
Nano <luxorius07@gmail.com>
2024-02-05 00:24:31 -0300

    agregamos el state tarea cuando estamos cargando una tarea en el formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,14 +1,30 @@
-import { Button } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
-import ListaTareas from './ListaTareas';
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import ListaTareas from "./ListaTareas";
+useState;
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+
   return (
     <section>
       <Form>
-        <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
-          <Form.Control type="text" placeholder="Ejemplo: Tarea" minLength={3} maxLength={50}/>
-          <Button variant="dark" className='ms-2' type ='submit'>Agregar</Button>
+        <Form.Group
+          className="mb-3 d-flex"
+          controlId="exampleForm.ControlInput1"
+        >
+          <Form.Control
+            type="text"
+            placeholder="Ejemplo: Tarea"
+            minLength={3}
+            maxLength={50}
+            onChange={(e)=> setTarea(e.target.value) }
+            value={tarea}
+          />
+          <Button variant="dark" className="ms-2" type="submit">
+            Agregar
+          </Button>
         </Form.Group>
       </Form>
       <ListaTareas></ListaTareas>
